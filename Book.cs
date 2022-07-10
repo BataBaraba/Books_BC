e77fa078aabd7704c936ba37dcd4458ce81f832b
BataBaraba <nmvujic@gmail.com>
2022-07-10 09:19:31 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Book
+    {
+        public int id { get; set; }
+        public string display_name { get; set; }
+        public string abbr { get; set; }
+        public string source_name { get; set; }
+        public Meta meta { get; set; }
+        public string parent_name { get; set; }
+        public int? book_parent_id { get; set; }
+        public int? affiliate_id { get; set; }
+    }
+
+    public class Deeplink
+    {
+        public bool has_multi { get; set; }
+        public bool is_supported { get; set; }
+    }
+
+    public class Logos
+    {
+        public string promo { get; set; }
+        public string primary { get; set; }
+        public string thumbnail { get; set; }
+        public string betslip_carousel { get; set; }
+    }
+
+    public class Meta
+    {
+        public Logos logos { get; set; }
+        public List<string> states { get; set; }
+        public bool is_promoted { get; set; }
+        public string website { get; set; }
+        public Deeplink deeplink { get; set; }
+        public bool? is_legal { get; set; }
+        public bool? is_preferred { get; set; }
+        public string primary_color { get; set; }
+        public int? betsync_status { get; set; }
+        public string secondary_color { get; set; }
+        public bool? is_fastbet_enabled_app { get; set; }
+        public bool? is_fastbet_enabled_web { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Book> books { get; set; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+using System.ComponentModel;
+using System.IO;
+
+namespace Books
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            WebClient client = new WebClient();
+            string strPageCode = client.DownloadString("https://api.actionnetwork.com/web/v1/books");
+
+            Root dobj = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(strPageCode);
+
+            //Console.WriteLine(dobj.books.Count);
+
+            for (int i = 0; i < dobj.books.Count; i++)
+            {
+                if (dobj.books[i].parent_name == null)
+                {
+                    dobj.books.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            //Console.WriteLine(dobj.books.Count);
+
+            for (int i = 0; i < dobj.books.Count; i++)
+            {
+                var counter = 0;
+                foreach (string s in dobj.books[i].meta.states.ToList())
+                {
+                    if(s == "NJ" || s == "CO")
+                    {
+                        counter++;
+                    }
+                }
+
+                if (counter == 0)
+                {
+                    dobj.books.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            //Console.WriteLine(dobj.books.Count);
+
+            //for (int i = 0; i < dobj.books.Count; i++)
+            //{
+            //    Console.WriteLine(dobj.books[i].id);
+            //    Console.WriteLine(dobj.books[i].parent_name);
+            //    Console.WriteLine(dobj.books[i].display_name);
+            //    foreach (string s in dobj.books[i].meta.states.ToList())
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+
+            //}
+
+            var query = dobj.books.GroupBy(book => book.parent_name)
+                              .Select(group =>
+                                    new {
+                                        parent_name = group.Key,
+                                        Books = group.OrderByDescending(x => x.parent_name)
+                                    })
+                              .OrderBy(group => group.Books.First().parent_name);
+
+            using (StreamWriter writetext = new StreamWriter("c:\\NENAD\\result.txt"))
+            //using (StreamWriter writetext = File.AppendText("c:\\NENAD\\result.txt"))
+            {
+
+                foreach (var group in query)
+                {
+                    writetext.WriteLine("{0}", "{" + group.parent_name + "}");
+                    foreach (var book in group.Books)
+                    {
+                        writetext.Write("{0}", "{" + book.display_name + "}");
+                        int index = dobj.books.FindIndex(a => a.id == book.id);
+                        writetext.Write("{");
+                        foreach (string s in dobj.books[index].meta.states.ToList())
+                        {
+                            writetext.Write(s + ",");
+                            //File.AppendAllText(@"c:\\NENAD\\result.txt", s + ",");
+                            //using (StreamWriter w = File.AppendText("c:\\NENAD\\result.txt"))
+                            //{
+                            //    writetext.WriteLine("{0}", "{" + s + "}");
+                            //}
+                        }
+                        writetext.Write("}" + "\n");
+                    }
+
+                }
+            }
+            string text = File.ReadAllText("c:\\NENAD\\result.txt");
+            text = text.Replace(",}", "}");
+            File.WriteAllText("c:\\NENAD\\result.txt", text);
+
+            //string text2 = File.ReadAllText("c:\\NENAD\\result.txt");
+            //text2 = text2.Replace("{" + "\r", "{");
+            //File.WriteAllText("c:\\NENAD\\result.txt", text2);
+        }
+    }
+}
